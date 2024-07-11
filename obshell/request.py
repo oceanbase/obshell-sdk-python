65c60fbf2c94c0866f4749e2fe7c48f476a5c56c
@ganquan.gq <ganquan.gq@oceanbase.com>
2024-07-11 15:10:28 +0800

    quote uri when request

--- obshell/request.py
+++ obshell/request.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import urllib
+
 
 class BaseRequest:
     def __init__(self, uri: str,
@@ -27,7 +29,7 @@ class BaseRequest:
             data = {}
         if headers is None:
             headers = {}
-        self.uri = uri
+        self.uri = urllib.parse.quote(uri)
         self.method = method
         self.host = host
         self.port = port
